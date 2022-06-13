549c37b42776e6abb78fc5e23d07898dfee5c7e9
unknown <fredericreact@gmail.com>
2022-06-14 02:05:54 +0800

    module export

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname +"/date.js");
+
+
 
 const app = express();
 
@@ -13,15 +16,7 @@ app.use(express.static("public"));
 
 app.get("/", function(req, res) {
 
-  let today = new Date();
-
-  let options ={
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  };
-
-  let day = today.toLocaleDateString("en-US", options);
+let day = date.getDay();
 
 
 
